ef9b98df9e0ce4a51bc5c4c7cbb86f0c900e98d4
daniellehausler <danielle@pagaya.com>
2020-03-05 21:47:11 +0200

    fixed active learner , added experiment run file

---
+++ run_experiment.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from nlp_active_learning.active_learning import ActiveLearner
+from nlp_active_learning.sample_methods import *
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# TODO :
+#  1. add pre process part ,
+#  2. add more sample methods,
+#  3. add a method to run experiments and save results,
+#  4. parallel run of multiple experiments
+
+DATA_SET = r'data_with_vectors\imdb_labelled.parquet'
+N_ITER = 5
+
+data = pd.read_parquet(DATA_SET)
+sentences = [np.array(sent.tolist()) for sent in data.embedded.tolist()]
+labels = data.Label.values.tolist()
+
+X_train, X_test, y_train, y_test = train_test_split(sentences, labels, test_size=0.2)
+
+# RF classifier example:
+learner = ActiveLearner(
+    clf=RandomForestClassifier(),
+    initialization_method=group_cosine_distance_mean,
+    n_samples=100
+)
+
+# learning loop:
+for i in range(N_ITER):
+
+    sampled_index = learner.add_n_new_samples(sample_method=cosine_distance_mean)
+    X_train = np.delete(X_train, sampled_index)
+    y_train = np.delete(y_train, sampled_index)
+    learner.fit_model()
+    learner.accuracy_score(X_test, y_test)
+    learner.f1_score(X_test, y_test)
+
+# results:
+f1 = learner.get_f1_scores()
+accuracy = learner.get_accuracy_scores()
